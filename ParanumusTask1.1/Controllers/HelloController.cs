938eebb48d16429ab29c7b0f842596cb8741fdf9
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-03-21 02:17:01 +0300

    Add project files.

---
+++ ParanumusTask1.1/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParanumusTask1._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+
+        [HttpGet("/")]
+        public IActionResult Hello()
+        {
+
+            return Content("Hello world");
+        }
+    }
+}
